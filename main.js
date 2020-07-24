e9fd2bdc0844cd7ca16616def48a1e57ecae96d7
TheAnswer16 <64368305+TheAnswer16@users.noreply.github.com>
2020-07-24 19:56:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const botaoNav = $("#menu-open");
 const mobileNav = $(".nav-mobile");
 const closeNav = $(".nav-close");
+const ulMobile = $(".ul-mobile");
 
 
 botaoNav.on("click", function(){
-    mobileNav.show();
+    mobileNav.addClass("visible");
+    ulMobile.addClass("visible-ul");
+    $("header").addClass("nav-open");
+    $("section").addClass("nav-open");
+
 });
 
 closeNav.on("click", function(){
-    mobileNav.hide();
+    mobileNav.removeClass("visible");
+    ulMobile.removeClass("visible-ul");
+    $("header").removeClass("nav-open");
+    $("section").removeClass("nav-open");
 });
 
